80fa798ef4db9432bd39d2c7759a37c20ac0e1e1
Daniel <dgarwood@uncc.edu>
2018-10-03 11:39:28 -0400

    Added code to trim irregular white space and styled layout better

--- src/components/VHDLGenerator.vue
+++ src/components/VHDLGenerator.vue
@@ -4,8 +4,8 @@
     <div class="columns">
 
       <div class="column is-5">
-        <h3>Upload a file</h3>
-        <div class="file has-name is-inline-block">
+        <h3>Upload a CSV</h3>
+        <div class="file has-name">
           <label class="file-label">
             <input class="file-input" type="file" accept=".csv" id="fileItem" @change="loadCSV">
             <span class="file-cta">
@@ -19,23 +19,24 @@
             </span>
           </label>
         </div>
-        <span>Delay:</span>
-        <input type="number" min="1" class="input number-input" v-model="delay">
-        <hr><p>OR</p><hr>
+        <hr>
         <h3>Paste from table</h3>
         <div class="input-values-textarea">
-          <textarea v-model="inputValues" class="textarea" rows="12" placeholder="Paste text from input table"></textarea>
+          <textarea v-model="inputValues" class="textarea" rows="8" placeholder="Paste text from input table"></textarea>
           <hr>
+          <label>Delay:</label>
+          <input type="number" min="1" class="input number-input" v-model="delay">
+          <br><br>
           <label class="checkbox">
             <input style="margin-right: 15px" v-model="addPrefix" type="checkbox">Add "SOURCE_" before port names
           </label>
-          <br><br>
-          <button class="button is-primary" style="margin-right: 15px" :disabled="!file" @click="parseCSV">Convert Uploaded CSV</button>
-          <button class="button is-info" :disabled="inputValues.length < 1" @click="parseTextArea">Convert Text</button>
+            <br><hr>
+            <button class="button is-primary" style="margin-right: 15px" :disabled="!file" @click="parseCSV">Convert Uploaded CSV</button>
+            <button class="button is-info" :disabled="inputValues.length < 1" @click="parseTextArea">Convert Text</button>
         </div>
       </div>
 
-      <div class="column is-7">
+      <div class="column is-6 is-offset-1">
         <div v-if="sourceCode.length > 0" class="code">
           <h2>Generated Code</h2>
           <p v-html="sourceCode"></p>
@@ -77,13 +78,19 @@ export default {
     },
     parseTextArea() {
       if (this.inputValues.length > 0) {
-        Papa.parse(this.inputValues.trim(), {
+        Papa.parse(this.inputValues, {
           complete: results => {
-            let data = results.data;
+            // removes the parsed blank lines
+            let data = results.data.filter(el => {
+              return el[0].length > 0;
+            });
+
+            // splits string of imput values into an array
             for (let i = 0; i < data.length; i++) {
               data[i] = data[i][0].trim().split(" ");
             }
 
+            // puts port name into the beginning of the corresponding array of inputs
             for (let i = 0; i < data.length - 1; i++) {
               if (i % 2 == 0) {
                 let portName = data[i][0];
@@ -91,9 +98,11 @@ export default {
               }
             }
 
+            // removes the arrays with only the port name
             data = data.filter(el => {
               return el.length > 1;
             });
+
             this.sourceCode = this.getSourceCode(data);
           }
         });
@@ -120,31 +129,33 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.column {
+  margin: 0 auto;
+}
+
 .vhdl-generator {
   margin-top: 25px;
   width: 80%;
+  font-size: 0.8rem;
 }
 
 .file {
   margin-right: 30px;
+}
 
-  label {
-    border-radius: 4px;
-  }
+label {
+  border-radius: 4px;
+  vertical-align: middle;
 }
 
 textarea {
-  margin-top: 25px;
-  font-size: 0.8rem;
+  font-size: 0.7rem;
 }
 
 .number-input {
-  width: 5em;
+  width: 4.5em;
   margin-left: 15px;
-}
-
-.code {
-  margin-left: 50px;
-  text-align: left !important;
+  vertical-align: middle;
+  font-size: 0.8rem;
 }
 </style>
